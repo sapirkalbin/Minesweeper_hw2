7b36fcd5b668fa45633a5403799d182d7e711cd9
sapir kalbin <sapirkalbin@sapirs-iMac.local>
2019-03-25 10:48:29 -0700

    Initial Commit

---
+++ Minesweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minesweeper
+//
+//  Created by sapir kalbin on 25/03/2019.
+//  Copyright © 2019 sapir kalbin and eti okonsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
